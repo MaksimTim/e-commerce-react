fb5dcb3a8ed6b661b9a07cc116bf01b244cfee3b
MaksimTim <maksim.snowden@gmail.com>
2022-10-27 19:51:22 +0300

    17 redux thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import CategoryPreview from "../../components/category-preview/category-preview.components";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from "../../store/categories/categories.selector";
+import Spinner from "../../components/spinner/spinner.component";
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      )}
     </>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,21 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { useEffect } from "react";
-import { getCollectionAndDocuments } from "../../utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
-import { setCategories } from "../../store/categories/categories.action";
+import {fetchCategoriesAsync} from "../../store/categories/categories.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCollectionAndDocuments();
-
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+      dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
